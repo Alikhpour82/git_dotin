cc78339967b88a259f94d3eebf7826b937eaa238
Ali Khadempour <ali.khpourarani823@gmail.com>
2023-06-07 22:09:18 +0330

    second commit

--- program.cs
+++ program.cs
@@ -2,4 +2,5 @@ void Main()
 {
     System.Console.WriteLine("Hello wolrd!");
     System.Console.WriteLine("this is the second message");
+    System.Console.WriteLine("this is the second commit")
 }
